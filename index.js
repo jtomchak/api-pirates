3fc447e1880ee5af8a230a7c69400f15454bf470
Jesse Tomchak <jtomchak@gmail.com>
2018-01-10 11:13:28 -0700

    WIP working

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 const path = require("path");
 const express = require("express");
-const sqlite = require("sqlite3").verbose();
+const Sequelize = require("sequelize");
+var models = require("./models");
 
 const app = express();
-
-//Connect to your DB
-const db = new sqlite.Database("./deadSeas.sqlite", err => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("........Connected to The DeadSea, arrrrrrgh.");
-});
 
 app.use(require("body-parser")());
 const handlebars = require("express-handlebars").create({
   defaultLayout: "main"
 });
-//const handlebars = require('express-handlebars');
-//handlebars.create({defaultLayout: 'main'});
 
 app.engine("handlebars", handlebars.engine);
 app.set("views", path.join(__dirname, "views"));
@@ -78,10 +69,14 @@ app.use((req, res) => {
   res.render("404");
 });
 
-app.listen(app.get("port"), () => {
-  console.log(
-    "Express started on http://localhost:" +
-      app.get("port") +
-      "; press Ctrl-C to terminate."
-  );
+//Connect to your DB
+// sync() will create all table if they doesn't exist in database
+models.sequelize.sync().then(function() {
+  app.listen(app.get("port"), () => {
+    console.log(
+      "Express started on http://localhost:" +
+        app.get("port") +
+        "; press Ctrl-C to terminate."
+    );
+  });
 });
