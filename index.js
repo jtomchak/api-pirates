3bfbbea65dccc9279cc4f153edd3aa989e6b64b2
Jesse Tomchak <jtomchak@gmail.com>
2018-04-23 14:25:19 -0700

    ops

--- index.js
+++ index.js
@@ -1,54 +1,37 @@
 const path = require("path");
 const express = require("express");
 
-const app = express();
+const app = express(); //init our express app
+//body-parser will take http request body and attach it
+//to the request object automatticly for us
 app.use(require("body-parser")());
 
+//Configuring the app to use the right templeting engine
 const handlebars = require("express-handlebars").create({
   defaultLayout: "main"
 });
-//const handlebars = require('express-handlebars');
-//handlebars.create({defaultLayout: 'main'});
 
 app.engine("handlebars", handlebars.engine);
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views")); //where are the views?
 app.set("view engine", "handlebars");
 
 app.set("port", process.env.PORT || 3000);
 
+//Routing Town!!!
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index"); //render the file in views named 'index'
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-app.get("/ship", (req, res) => {
-  res.render("ship");
+  res.render("about"); //render the file in views named 'about'
 });
 
 app.get("/treasure", (req, res) => {
   res.render("treasure");
 });
 
-app.get("/pirate", (req, res) => {
-  res.render("pirate-form");
-});
-
-app.post("/pirate", (req, res) => {
-  console.log(req.body);
-  res.send("Thanks");
-});
-
-app.use((req, res) => {
-  res.render("404");
-});
-
 app.listen(app.get("port"), () => {
   console.log(
-    "Express started on http://localhost:" +
-      app.get("port") +
-      "; press Ctrl-C to terminate."
+    "Express started on http://localhost:" + app.get("port") + "; press Ctrl-C to terminate."
   );
 });
